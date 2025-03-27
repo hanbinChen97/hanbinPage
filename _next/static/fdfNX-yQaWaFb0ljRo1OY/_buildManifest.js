c092397c508b656d924ba1cffb783b5edaacd731
hanbin chen <hanbin.9797@gmail.com>
2025-03-27 13:38:39 +0100

    Updates

---
+++ _next/static/fdfNX-yQaWaFb0ljRo1OY/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/953-2a1531deb53a0eba.js","static/chunks/pages/index-f55f5bac47b7f6f4.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
